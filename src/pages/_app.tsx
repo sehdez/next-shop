8534b4d07c54d2a0e2e686d2c48e62caea0db647
sehdez <sergio.hdez.cuevas@gmail.com>
2023-06-07 17:48:50 -0600

    Pagos con tdc y paypal agregados

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,35 @@
 import type { AppProps } from 'next/app'
-import { SessionProvider } from "next-auth/react"
-import { lightTheme } from '@/themes'
-import { AuthProvider, CartProvider, UiProvider } from '@/context'
+import { SessionProvider } from 'next-auth/react'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { SWRConfig } from 'swr'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+
+import { lightTheme } from '@/themes'
+import { AuthProvider, CartProvider, UiProvider } from '@/context'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <SessionProvider>
-            <SWRConfig
-            value={{
-                // refreshInterval: 3000,
-                fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
-            }}
-            >
-                <AuthProvider>
-                    <CartProvider>
-                        <UiProvider>
-                            <ThemeProvider theme={lightTheme}>
-                                <CssBaseline />
-                                <Component {...pageProps} />
-                            </ThemeProvider>
-                        </UiProvider>
-                    </CartProvider>
-                </AuthProvider>
-            </SWRConfig>
+            <PayPalScriptProvider options={{ 'client-id': process.env.NEXT_PUBLIC_PAYPAL_CLIENT || ''}}>
+                <SWRConfig
+                value={{
+                    // refreshInterval: 3000,
+                    fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+                }}
+                >
+                    <AuthProvider>
+                        <CartProvider>
+                            <UiProvider>
+                                <ThemeProvider theme={lightTheme}>
+                                    <CssBaseline />
+                                    <Component {...pageProps} />
+                                </ThemeProvider>
+                            </UiProvider>
+                        </CartProvider>
+                    </AuthProvider>
+                </SWRConfig>
+            </PayPalScriptProvider>
         </SessionProvider>
     )
 
