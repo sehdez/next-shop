87254a16d8d9aef240bda9d793806bded3f66abe
sehdez <sergio.hdez.cuevas@gmail.com>
2023-06-22 20:23:26 -0600

    Se agregó un loading de pantalla completa mientras se generan las vistan, para mejorar la UX

--- src/components/ui/FullScreenLoading.tsx
+++ src/components/ui/FullScreenLoading.tsx
@@ -1,18 +1,27 @@
 import { ShopLayout } from '@/components/layouts'
-import { Box, CircularProgress, Typography } from '@mui/material'
+import { Box, CircularProgress, Typography, CardMedia } from '@mui/material';
 
 export const FullScreenLoading = () => {
     return (
         <Box
-                sx={{ flexDirection: 'column' }}
-                display='flex'
-                justifyContent='center'
-                alignItems='center'
-                height='calc(100vh - 200px)'
-            >
-                <Typography variant='h2' fontWeight={200} sx={{ mb: 3 }}>Cargando...</Typography>
-                <CircularProgress />
-            </Box>
+            sx={{ 
+                flexDirection: 'column',
+                position: 'fixed',
+                top: 0,
+                left:0,
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'rgba(255, 255, 255, 0.7)', /* Fondo semitransparente */
+                zIndex:1101
+            }}
+            className='fadein'
+            display='flex'
+            justifyContent='center'
+            alignItems='center'
+            // height='calc(100vh - 200px)'
+        >
+            <div className='rotatingImage fadein'></div>
+        </Box>
     )
 }
 
